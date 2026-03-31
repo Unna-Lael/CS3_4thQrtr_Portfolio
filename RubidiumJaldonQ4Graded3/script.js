45f8da6afd28c9adca33ed573b321cdb20959e1d
Unna-Lael <b2029ultjaldon@pshs.edu.ph>
2026-03-31 15:52:40 +0800

    Update script.js

--- RubidiumJaldonQ4Graded3/script.js
+++ RubidiumJaldonQ4Graded3/script.js
@@ -1,14 +1,20 @@
 function plotPoint() {
     console.log(x0, y0, x, y) // checks the arguments passed to this function
+    var in1 = document.getElementById("in1");
+    var out1 = document.getElementById("out1");
     in1.innerHTML = x0 + " " + y0 + " " + x + " " + y;
     var point = document.createElement('div');  
     point.className = 'point';
-    /* control where to place the div on the screen using left and bottom of position:absolute */
-    point.style.left = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
-    point.style.bottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
-    document.getElementById('coordinatePlane').appendChild(point);
+    var plane = document.getElementById('coordinatePlane'); /* clear previous point so it doesnt stack */
+    plane.innerHTML = "";  /* control where to place the div on the screen using left and bottom of position:absolute */
+    point.style.left = (x - x0 + 200 - 5) + 'px';
+    point.style.bottom = (y - y0 + 200 - 5) + 'px';
+    plane.appendChild(point);
 
-    if (x > x0 && y > y0){
+    if (x == x0 || y == y0){
+      out1.innerHTML = "divisa"
+    }
+    else if (x > x0 && y > y0){
       out1.innerHTML = "NE"
     }
     else if (x < x0 && y < y0){
@@ -19,8 +25,5 @@ function plotPoint() {
     }
     else if (x < x0 && y > y0){
       out1.innerHTML = "NO"
-    }
-    else{
-      out1.innerHTML = "divisa"
     }
 }
